a083cd3b87e4a4eeb338d8d184f6baa4ebfbb6a7
DELLHENRIQUE\Administrador <luis.freitas@maximaseguranca.com.br>
2022-12-22 17:58:43 -0300

    Ajustes

---
+++ src/Utils/listaMeses.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        id: '1',
+        nomeMes: "Janeiro"
+    },
+    {
+        id: '2',
+        nomeMes: "Fevereiro"
+    },
+    {
+        id: '3',
+        nomeMes: "Março"
+    },
+    {
+        id: '4',
+        nomeMes: "Abril"
+    },
+    {
+        id: '5',
+        nomeMes: "Maio"
+    },
+    {
+        id: '6',
+        nomeMes: "Junho"
+    },
+    {
+        id: '7',
+        nomeMes: "Julho"
+    },
+    {
+        id: '8',
+        nomeMes: "Agosto"
+    },
+    {
+        id: '9',
+        nomeMes: "Setembro"
+    },
+    {
+        id: '10',
+        nomeMes: "Outubro"
+    },
+    {
+        id: '11',
+        nomeMes: "Novembro"
+    },
+    {
+        id: '12',
+        nomeMes: "Dezembo"
+    }
+]
